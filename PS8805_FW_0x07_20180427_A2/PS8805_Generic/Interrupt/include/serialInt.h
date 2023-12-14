d8f23866500bb7da7f4af9bc68d4c3cb12bf4fd0
Pingyao5115 <153799490+Pingyao5115@users.noreply.github.com>
2023-12-14 16:25:54 +0800

    Delete PS8805_FW_0x07_20180427_A2/PS8805_Generic directory

--- PS8805_FW_0x07_20180427_A2/PS8805_Generic/Interrupt/include/serialInt.h
+++
@@ -1,4 +0,0 @@
-#ifndef _SERIAL_INT_H_
-#define _SERIAL_INT_H_
-void CommInt(void);
-#endif
--- PS8805_FW_0x07_20180427_A2/PS8805_Generic/LL_Functions/include/ll_Functions.h
+++
@@ -1,7 +0,0 @@
-#ifndef _LL_MISC_FUNCTIONS_H_
-#define _LL_MISC_FUNCTIONS_H_
-	
-void ll_MPUInit(void);
-void ll_SetVerNum( void );
-
-#endif
